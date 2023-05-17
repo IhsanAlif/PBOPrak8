31aa71248edae02eeace1f8e6bbdcf69036d2919
IhsanAlif <100286370+IhsanAlif@users.noreply.github.com>
2023-05-17 19:29:10 +0700

    Add files via upload

---
+++ BangunDatarGeneric.java
@@ -0,0 +1,22 @@
+/*
+ * File : BangunDatarGeneric.java  17/05/2023
+ * Penulis : Ihsan Alif Lazuardi Anwar(24060121140162)
+ * Deskripsi : kelas konstruksi generic untuk BangunDatar
+*/
+
+public class BangunDatarGeneric<T extends BangunDatar> {
+    private T bangunDatar;
+
+    public void set(T tipeBangunDatar) {
+        bangunDatar = tipeBangunDatar;
+    }
+
+    public T get() {
+        return bangunDatar;
+    }
+
+    public double hitungKeliling() {
+        return bangunDatar.hitungKeliling();
+    }
+}
+
